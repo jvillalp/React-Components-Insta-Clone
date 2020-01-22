41fdcc381abdd46682602e74d1b13a277b20e8ff
Juana Villalpando <juana.v14@gmail.com>
2020-01-22 08:47:51 -0800

    MVP

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -15,7 +15,7 @@ const LikeSection = props => {
         <i className="far fa-comment"/>
       </div>
     </div>
-    <p className="like-number">{props.likes}</p>
+    <p className="like-number">{props.likes} likes</p>
 </div>
   )
 };
